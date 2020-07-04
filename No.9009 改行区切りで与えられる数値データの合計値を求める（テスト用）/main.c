2e95d96dc638101088f89ca36c06cd4bee9e61bb
toco8 <39761181+toco8@users.noreply.github.com>
2020-07-04 13:16:18 +0900

    Create main.c

---
+++ No.9009 改行区切りで与えられる数値データの合計値を求める（テスト用）/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int N;
+  unsigned long int k;
+  unsigned long int sum = 0;
+
+  scanf("%d", &N);
+  do {
+    scanf("%lu", &k);
+    sum += k;
+    N--;
+  } while (N > 0);
+  printf("%lu\n", sum);
+  return 0;
+}
